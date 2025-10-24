a669ff380f35f7dbd557aa442db3755c2ea16e64
BernardoPatriarca <bernardopatriarca24@gmail.com>
2025-10-23 21:54:33 -0300

    aula 23-10-2025

--- the-moviebox-react/src/App.tsx
+++ the-moviebox-react/src/App.tsx
@@ -1,8 +1,13 @@
+import { Footer } from "./components/layout/footer";
+import { Header } from "./components/layout/header";
+
 function App() {
 
   return (
     <>
       <Header />
+      <main></main>
+      <Footer />
     </>
   )
 }
