37752cb325db1ad81b2794fc872f3f23d173ff9c
BernardoPatriarca <bernardopatriarca24@gmail.com>
2025-10-30 21:55:44 -0300

    aula 30/10/2025 2.0

--- the-moviebox-react/src/cases/movies/components/movie-content.tsx
+++ the-moviebox-react/src/cases/movies/components/movie-content.tsx
@@ -1,8 +1,16 @@
-import { useState } from "react"
-import type { MovieDTO } from "../services/moive.service"
+import { useEffect, useState } from "react"
+import { MovieService, type MovieDTO } from "../services/moive.service"
 
 export function MovieContent() {
-    const [movies, seMovies] = useState<MovieDTO[]>({})
+
+    const [movies, setMovies] = useState<MovieDTO[]>([])
+
+    useEffect(() => {
+        MovieService.list().then(result => {
+            setMovies(result)
+        })
+    }, []);
+
     return (
         <section className="bg-[#1c1c1c] p-8 grid grid-cols-[repeat(auto-fill, minmax(200px, 1fr))] gap-8 max-w-6xl x-auto">
             <p>{JSON.stringify(movies)}</p>
