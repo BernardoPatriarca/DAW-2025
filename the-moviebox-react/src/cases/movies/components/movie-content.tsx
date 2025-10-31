431f73366acef1cd1d9a2a382687d9826b9de9fc
BernardoPatriarca <bernardopatriarca24@gmail.com>
2025-10-30 21:55:26 -0300

    aula 30/10/2025

---
+++ the-moviebox-react/src/cases/movies/components/movie-content.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react"
+import type { MovieDTO } from "../services/moive.service"
+
+export function MovieContent() {
+    const [movies, seMovies] = useState<MovieDTO[]>({})
+    return (
+        <section className="bg-[#1c1c1c] p-8 grid grid-cols-[repeat(auto-fill, minmax(200px, 1fr))] gap-8 max-w-6xl x-auto">
+            <p>{JSON.stringify(movies)}</p>
+        </section>
+    )
+}
